a1fb4304c4213ab11ab404c92b8ab69fac3e4068
hareesh-r <hareeshprogrammer@gmail.com>
2022-05-15 01:32:46 +0530

    feat : Added artist info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import logo from "./assets/img/logo.png";
 import title from "./assets/img/title.png";
 import quote from "./assets/img/quote.jpeg";
+import varun from "./assets/img/varun.jpeg";
+import sneha from "./assets/img/sneha.PNG";
+
 
 function App() {
+  const [galleryImageURLList, setGalleryImageURLList] = useState([
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/243170819_117782853966443_1084189875318892950_n.jpg?alt=media&token=3ca0a650-3d41-4aa5-8f0e-bea13aa3d434",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240446983_1857483884413029_7090633982480615246_n.jpg?alt=media&token=b483e7e7-3ad7-442d-8164-d9415fa02b5c",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240338643_581961716141133_7201891540458121396_n.jpg?alt=media&token=103f9fbf-5746-4f7e-9e60-d21601d55029",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240229360_4223454017749267_3978895926170855874_n.jpg?alt=media&token=7c6632cd-dad7-40a2-b6a0-215bcf8618f9",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240111252_382058380051507_2652896964985804596_n.jpg?alt=media&token=66e6518a-b259-4c35-99c8-0c1bdb057d49",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/239662045_246369054010535_374104280886273195_n.jpg?alt=media&token=0a1c2be4-f37b-44da-8a69-2ba07a0050b6",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/239629760_356968529249046_489682444598649654_n.jpg?alt=media&token=8b007107-9fcd-4bd7-a4a7-3031daabaf45",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/230871392_795601804459738_5893987971165873900_n.jpg?alt=media&token=81cd0fb3-d4fa-49eb-a6ca-f2947bdf30b2",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/213582965_193176599316299_9076140851186245588_n.jpg?alt=media&token=5e3653ef-67df-4a7d-ad6a-ef8e31da7fc7",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/160058079_774663656792815_5763007212025251754_n.jpg?alt=media&token=4ffcf5ed-fcbc-44f1-ab6d-068cb28f1274",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/192169413_325674652406896_1923575219187121037_n.jpg?alt=media&token=bf2b7e81-2eab-40b3-833e-92f8098354f6",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/243170819_117782853966443_1084189875318892950_n.jpg?alt=media&token=3ca0a650-3d41-4aa5-8f0e-bea13aa3d434",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240446983_1857483884413029_7090633982480615246_n.jpg?alt=media&token=b483e7e7-3ad7-442d-8164-d9415fa02b5c",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240338643_581961716141133_7201891540458121396_n.jpg?alt=media&token=103f9fbf-5746-4f7e-9e60-d21601d55029",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240229360_4223454017749267_3978895926170855874_n.jpg?alt=media&token=7c6632cd-dad7-40a2-b6a0-215bcf8618f9",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/240111252_382058380051507_2652896964985804596_n.jpg?alt=media&token=66e6518a-b259-4c35-99c8-0c1bdb057d49",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/239662045_246369054010535_374104280886273195_n.jpg?alt=media&token=0a1c2be4-f37b-44da-8a69-2ba07a0050b6",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/239629760_356968529249046_489682444598649654_n.jpg?alt=media&token=8b007107-9fcd-4bd7-a4a7-3031daabaf45",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/230871392_795601804459738_5893987971165873900_n.jpg?alt=media&token=81cd0fb3-d4fa-49eb-a6ca-f2947bdf30b2",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/213582965_193176599316299_9076140851186245588_n.jpg?alt=media&token=5e3653ef-67df-4a7d-ad6a-ef8e31da7fc7",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/160058079_774663656792815_5763007212025251754_n.jpg?alt=media&token=4ffcf5ed-fcbc-44f1-ab6d-068cb28f1274",
+    "https://firebasestorage.googleapis.com/v0/b/heaven-tattoo.appspot.com/o/192169413_325674652406896_1923575219187121037_n.jpg?alt=media&token=bf2b7e81-2eab-40b3-833e-92f8098354f6",
+  ]);
   return (
     <div className="App">
       <div className="top-line sticky"></div>
@@ -26,6 +53,11 @@ function App() {
           </a>
         </div>
         <div className="nav-item">
+          <a className="inactive" href="#info">
+            Artists
+          </a>
+        </div>
+        <div className="nav-item">
           <a className="inactive" href="#bookings">
             Bookings
           </a>
@@ -38,23 +70,23 @@ function App() {
             <strong className="gold">Hours</strong>
             <div className="flex row">
               <strong>Mon-Fri</strong>
-              <p className="time lite-grey">8AM-7PM</p>
+              <p className="time lite-grey">10:00 AM-09:30 PM</p>
             </div>
             <div className="flex row">
               <strong>Sat-Sun</strong>
-              <p className="time lite-grey">8AM-12AM</p>
+              <p className="time lite-grey">11:00 AM-06:00 PM</p>
             </div>
           </div>
           <div className="left-home flex col">
             <strong className="gold">Address</strong>
             <div className="left-home flex col">
-              <strong>Area</strong>
-              <p className="lite-grey">Specific</p>
+              <strong>Thondiarpet , Chennai</strong>
+              <p className="lite-grey">No 18/4 Kasipuram B Block</p>
             </div>
           </div>
           <div className="left-home flex col">
             <strong className="gold">Call</strong>
-            <p>958647589</p>
+            <p>+91 73054 53997</p>
           </div>
         </div>
         <div className="right-home flex col">
@@ -74,57 +106,39 @@ function App() {
             <a href="#gallery">
               <button>View Galleries</button>
             </a>
-            <a href="#bookings">
+            <a href="#info">
               <button>Get a Quote</button>
             </a>
           </div>
         </div>
       </div>
       <div id="gallery" className="gallery flex col">
+        <div className="side-title side-title-gallery">
+          <h1>Galleries</h1>
+          <div className="dot dot-1"></div>
+          <div className="dot dot-2"></div>
+        </div>
         <div className="black-box-top">
           <h1 className="gallery-title gold">Gallery</h1>
         </div>
         <div className="gallery-container flex">
           <div class="roww">
-            <div class="column">
-              <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-              <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-              <img src="https://www.w3schools.com/w3images/falls2.jpg" />
-              <img src="https://www.w3schools.com/w3images/paris.jpg" />
-              <img src="https://www.w3schools.com/w3images/nature.jpg" />
-              <img src="https://www.w3schools.com/w3images/mist.jpg" />
-              <img src="https://www.w3schools.com/w3images/paris.jpg" />
-            </div>
-            <div class="column">
-              <img src="https://www.w3schools.com/w3images/underwater.jpg" />
-              <img src="https://www.w3schools.com/w3images/ocean.jpg" />
-              <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-              <img src="https://www.w3schools.com/w3images/mountainskies.jpg" />
-              <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-              <img src="https://www.w3schools.com/w3images/underwater.jpg" />
-            </div>
-            <div class="column">
-              <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-              <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-              <img src="https://www.w3schools.com/w3images/falls2.jpg" />
-              <img src="https://www.w3schools.com/w3images/paris.jpg" />
-              <img src="https://www.w3schools.com/w3images/nature.jpg" />
-              <img src="https://www.w3schools.com/w3images/mist.jpg" />
-              <img src="https://www.w3schools.com/w3images/paris.jpg" />
-            </div>
-            <div class="column">
-              <img src="https://www.w3schools.com/w3images/underwater.jpg" />
-              <img src="https://www.w3schools.com/w3images/ocean.jpg" />
-              <img src="https://www.w3schools.com/w3images/wedding.jpg" />
-              <img src="https://www.w3schools.com/w3images/mountainskies.jpg" />
-              <img src="https://www.w3schools.com/w3images/rocks.jpg" />
-              <img src="https://www.w3schools.com/w3images/underwater.jpg" />
-            </div>
+            {galleryImageURLList.map((imageURL, index) => (
+              <div className="column">
+                <img src={imageURL} />
+              </div>
+            ))}
           </div>
         </div>
         <div className="black-box"></div>
       </div>
       <div id="info" className="info flex col">
+        <div className="side-title side-title-info">
+          <h1>Information</h1>
+          <div className="dot dot-1"></div>
+          <div className="dot dot-2"></div>
+          <div className="dot dot-3"></div>
+        </div>
         <div className="info-container flex row">
           <a href="upi://pay?pa=heavantattooz@okhdfcbank&pn=Booking%20@%20heaventattooz&tn=Booking%20for%20tattoo&am=35000&cu=INR">
             <div className="card flex col">
@@ -190,7 +204,36 @@ function App() {
           </a>
         </div>
       </div>
+      <div className="artist" id="artist">
+        <div className="side-title side-title-artist">
+          <h1>Artists</h1>
+          <div className="dot dot-1"></div>
+          <div className="dot dot-2"></div>
+          <div className="dot dot-3"></div>
+          <div className="dot dot-4"></div>
+        </div>
+        <div className="artist-container flex row">
+          <div className="artist-card flex col">
+            <img src={varun} alt="" />
+            <h1 className="artist-designation gold">Lead Male Tattooist</h1>
+            <h2 className="artist-name">Varun</h2>
+          </div>
+          <div className="artist-card flex col">
+            <img src={sneha} alt="" />
+            <h1 className="artist-designation gold">Lead Female Tattooist</h1>
+            <h2 className="artist-name">Sneha</h2>
+          </div>
+        </div>
+      </div>
       <div className="bookings flex col" id="bookings">
+        <div className="side-title side-title-bookings">
+          <h1>Bookings</h1>
+          <div className="dot dot-1"></div>
+          <div className="dot dot-2"></div>
+          <div className="dot dot-3"></div>
+          <div className="dot dot-4"></div>
+          <div className="dot dot-5"></div>
+        </div>
         <h1>Do the bookings via these methods</h1>
         <div className="bookings-container flex row">
           <div className="booking-item flex col sa">
@@ -220,7 +263,11 @@ function App() {
             </a>
           </div>
           <div className="booking-item flex col sa">
-            <a className="flex col" href="">
+            <a
+              className="flex col"
+              target="_blank"
+              href="https://www.instagram.com/heavan_tattooz/"
+            >
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"
                 alt=""
